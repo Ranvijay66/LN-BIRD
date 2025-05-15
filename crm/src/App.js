28d346d7c323345fa246fd51e929e28f2639dfc2
Ranvijay66 <web.digitaltec@gmail.com>
2025-05-15 18:22:19 +0530

    todays code

--- crm/src/App.js
+++ crm/src/App.js
@@ -1,61 +1,58 @@
-import React from 'react';
+import React from "react";
 import Header from "./components/Header";
-import ImageWithText from './components/ImageWithText';
-import Header2 from './components/Header2';
-import Body from './components/Body';
-import Body2 from './components/Body2';
-import Body3 from './components/Body3';
-import Footer from './components/Footer';
-import Form1 from '../src/components/Form';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './components/home';
-import Contact from '../src/components/Contact';  // Adjust the path based on your project structure
-import Account from './components/Account';
-import Deal from './components/Deal';
-import Solution from './components/Solution';
-import TaskForm from './components/Task';
-import Alllead from './components/Alllead';
-import Allcontact from './components/Allcontact';
-import Allaccount from './components/Allaccount';
-import Alldeals from './components/Alldeals';
-import Alltask from './components/Alltask';
-import Allmeeting from './components/Allmeeting';
-import AllServices from './components/Allservices';
-import Project from './components/Project';
-import Call from './components/Call';
-import Report from './components/Report';
-
+import ImageWithText from "./components/ImageWithText";
+import Header2 from "./components/Header2";
+import Body from "./components/Body";
+import Body2 from "./components/Body2";
+import Body3 from "./components/Body3";
+import Footer from "./components/Footer";
+import Form1 from "../src/components/Form";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/home";
+import Contact from "../src/components/Contact"; // Adjust the path based on your project structure
+import Account from "./components/Account";
+import Deal from "./components/Deal";
+import Solution from "./components/Solution";
+import TaskForm from "./components/Task";
+import Alllead from "./components/Alllead";
+import Allcontact from "./components/Allcontact";
+import Allaccount from "./components/Allaccount";
+import Alldeals from "./components/Alldeals";
+import Alltask from "./components/Alltask";
+import Allmeeting from "./components/Allmeeting";
+import AllServices from "./components/Allservices";
+import Project from "./components/Project";
+import Call from "./components/Call";
+import Report from "./components/Report";
+import Allreport from "./components/Allreport";
+import Myreport from "./components/Myreport";
 
 function App() {
   return (
     <div>
-    <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<Home/>}></Route>
-      <Route path='/leads' element={<Form1/>}></Route>
-      <Route path='/Contact' element={<Contact/>}></Route>
-      <Route path='/Account' element={<Account/>}></Route>
-      <Route path='/Deal' element={<Deal/>}></Route>
-      <Route path='/Solution' element={<Solution/>}></Route>
-      <Route path='/Task' element={<TaskForm/>}></Route>
-      <Route path='/Alllead' element={<Alllead/>}></Route>
-      <Route path='/Allcontact' element={<Allcontact/>}></Route>
-      <Route path='/Allaccount' element={<Allaccount/>}></Route>
-      <Route path='/Alldeals' element={<Alldeals/>}></Route>
-      <Route path='/Alltask' element={<Alltask/>}></Route>
-      <Route path='/Allmeeting' element={<Allmeeting/>}></Route>
-      <Route path='/Allservices' element={<AllServices/>}></Route>
-      <Route path='/Project' element={<Project/>}></Route>
-       <Route path='/Call' element={<Call/>}></Route>
-       <Route path='/Report' element={<Report/>}></Route>
-
-
-
-
-
-
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/leads" element={<Form1 />}></Route>
+          <Route path="/Contact" element={<Contact />}></Route>
+          <Route path="/Account" element={<Account />}></Route>
+          <Route path="/Deal" element={<Deal />}></Route>
+          <Route path="/Solution" element={<Solution />}></Route>
+          <Route path="/Task" element={<TaskForm />}></Route>
+          <Route path="/Alllead" element={<Alllead />}></Route>
+          <Route path="/Allcontact" element={<Allcontact />}></Route>
+          <Route path="/Allaccount" element={<Allaccount />}></Route>
+          <Route path="/Alldeals" element={<Alldeals />}></Route>
+          <Route path="/Alltask" element={<Alltask />}></Route>
+          <Route path="/Allmeeting" element={<Allmeeting />}></Route>
+          <Route path="/Allservices" element={<AllServices />}></Route>
+          <Route path="/Project" element={<Project />}></Route>
+          <Route path="/Call" element={<Call />}></Route>
+          <Route path="/Report" element={<Report />}></Route>
+          <Route path="/Allreport" element={<Allreport />}></Route>
+           <Route path="/Myreport" element={<Myreport />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
     // <>
     //   <Header />
@@ -66,7 +63,7 @@ function App() {
     //   <Body3/>
     //   <Footer/>
     //   <Form1/>
-      
+
     // </>
   );
 }
--- crm/src/components/Myreport.js
+++ crm/src/components/Myreport.js
@@ -3,13 +3,11 @@ import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
-
 import Header from "./Header";
-
 import { useNavigate } from "react-router-dom";
 
-function Report() {
-   const navigate = useNavigate();
+function Myreport() {
+  const navigate = useNavigate();
   const [activeFolder, setActiveFolder] = useState(null);
 
   const [searchTerm, setSearchTerm] = useState("");
@@ -25,171 +23,10 @@ function Report() {
     <div style={{ width: "100%" }}>
       <Header />
 
-      <div
-        style={{ backgroundColor: "green", marginTop: "6px", padding: "10px" }}
-      >
-        {/* Flex container for dropdowns */}
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          {/* First dropdown aligned to the left */}
-          <div className="dropdown">
-            <button
-              className="btn btn-secondary dropdown-toggle"
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              All Meetings
-            </button>
-            <ul className="dropdown-menu">
-              <li>
-                <button className="dropdown-item" type="button">
-                  Action
-                </button>
-              </li>
-              <li>
-                <button className="dropdown-item" type="button">
-                  Another action
-                </button>
-              </li>
-              <li>
-                <button className="dropdown-item" type="button">
-                  Something else here
-                </button>
-              </li>
-            </ul>
-          </div>
+      <h5 style={{ marginLeft:"350px",marginTop:"20px",color:"green" }}>My Report</h5>
 
-          {/* Container for last three buttons with gap of 10px */}
-          <div style={{ display: "flex", gap: "10px" }}>
-            {/* Second dropdown aligned to the right */}
-            <div className="dropdown">
-              <button
-                className="btn btn-secondary dropdown-toggle d-flex align-items-center justify-content-center"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                style={{
-                  width: "auto", // Adjust button width based on content
-                  lineHeight: ".4",
-                  whiteSpace: "pre-line",
-                  fontFamily: "monospace",
-                  textAlign: "center", // Horizontal center for text
-                  padding: "8px 0", // Optional: tighter vertical spacing
-                }}
-              >
-                • ━━━━━{"\n"}• ━━━━━{"\n"}• ━━━━━
-              </button>
 
-              <ul
-                className="dropdown-menu"
-                style={{ position: "absolute", right: 0 }}
-              >
-                <li>
-                  <button className="dropdown-item" type="button">
-                    List View
-                  </button>
-                </li>
-                <li>
-                  &nbsp;&nbsp;&nbsp;&nbsp;<b>Canvas View</b>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Custom View List
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Tile View
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Table View
-                  </button>
-                </li>
-              </ul>
-            </div>
-
-            {/* Third dropdown */}
-            <div className="dropdown">
-              <button
-                className="btn btn-primary dropdown-toggle"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Create Lead
-              </button>
-              <ul className="dropdown-menu">
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Import Leads
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Import Notes
-                  </button>
-                </li>
-              </ul>
-            </div>
-
-            {/* Fourth dropdown */}
-            <div className="dropdown">
-              <button
-                className="btn btn-secondary dropdown-toggle"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Actions
-              </button>
-              <ul className="dropdown-menu">
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Mass Delete
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Mass Update
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Mass Email
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Approve Leads
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Add to Campaigns
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Export Leads
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Zoho sheet view
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Print View
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+     
 
       {/* Wrapper with flex layout */}
       <div
@@ -204,18 +41,20 @@ function Report() {
         <div
           style={{
             flex: 1,
+
+             
             display: "flex",
             borderRadius: "10px",
             flexDirection: "column",
             gap: "10px",
-            overflowY: "auto",
-            backgroundColor: "white",
+           // overflowY: "auto",
+           // backgroundColor: "dark",
           }}
         >
           {/* First Dropdown */}
 
           <div style={styles.container}>
-            <h5 style={styles.header}>Folders</h5>
+            <h5 style={{ ...styles.header, color: "green" }}><b>Folders</b></h5>
 
             <input
               type="text"
@@ -251,7 +90,7 @@ function Report() {
                       ? styles.activeItem
                       : styles.item
                   }
-                  onClick={() => navigate("Myreport")}
+                  onClick={() => navigate("/Myreport")}
                 >
                   My Reports
                 </li>
@@ -369,25 +208,29 @@ function Report() {
         </div>
 
         {/* Right Section with scrollable table */}
+
+        
         <div
           style={{
-            flex: 3,
-            paddingLeft: "20px",
+           
+            paddingLeft: "10px",
             maxHeight: "500px",
             overflowY: "auto",
           }}
         >
-          <table className="table table-striped table-bordered">
+          <table
+  className="table table-striped table-bordered"
+  style={{ marginRight: "200px" }}
+>
+
             <thead className="table-yellow">
               <tr>
                 <th style={{ width: "40px" }}></th>
-                <th>Subject</th>
-                <th>Call Type</th>
-                <th>Call start time</th>
-                <th>Call Duration </th>
-                <th>Related To</th>
-                <th>Contact Name</th>
-                <th>Call Owner</th>
+                <th style={{color:"green"}}>Report Name</th>
+                <th style={{color:"green"}}>Description</th>
+                <th style={{color:"green"}}>Folder</th>
+                <th style={{ fontSize: "15px",color:"green"} }>Last Accessed Date</th>
+                <th style={{fontSize:"15px",color:"green"}}>Created By</th>
               </tr>
             </thead>
             <tbody>
@@ -395,14 +238,14 @@ function Report() {
                 <td>
                   <input type="checkbox" />
                 </td>
-                <td>Follow Up With Lead</td>
-                <td>Out bound</td>
-                <td>20/03/2025 12:47 PM</td>
-                <td></td>
-                <td>Chau Kitzman(sample)</td>
-                <td></td>
-                <td>dev.papayapalette</td>
+                <td>NO RECORD FOUND</td>
+                <td>NO RECORD FOUNDe</td>
+                <td>NO RECORD FOUND</td>
+                <td>NO RECORD FOUND</td>
+                <td>-</td>
               </tr>
+
+              
             </tbody>
           </table>
         </div>
@@ -413,11 +256,12 @@ function Report() {
 
 const styles = {
   container: {
-    width: "250px",
+    width: "300px",
     backgroundColor: "#f5f5f5",
     padding: "10px",
     borderRight: "1px solid #ccc",
     fontFamily: "Arial, sans-serif",
+     overflowY: "auto",
   },
   header: {
     marginBottom: "10px",
@@ -443,4 +287,4 @@ const styles = {
   },
 };
 
-export default Report;
+export default Myreport;
